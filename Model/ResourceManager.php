694f8ced2853e49093335accae30cf548f50fe73
Jason Pancake <jason@jasonpancake.com>
2014-10-29 17:09:22 -0400

    added type hint for logger

--- Model/ResourceManager.php
+++ Model/ResourceManager.php
@@ -4,13 +4,14 @@ namespace FJL\ChargifyBundle\Model;
 
 use Guzzle\Http\ClientInterface;
 use Guzzle\Http\Exception\RequestException;
+use Psr\Log\LoggerInterface;
 
 abstract class ResourceManager
 {
     protected $client;
     protected $logger;
 
-    public function __construct(ClientInterface $client, $logger = null) {
+    public function __construct(ClientInterface $client, LoggerInterface $logger = null) {
         $this->client = $client;
         $this->logger = $logger;
     }
